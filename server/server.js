5c33183c0e10565abe66f805a64ad1540b05e905
danielaebazan <daniela.e.bazan@hotmail.com>
2024-06-13 11:41:46 -0600

    creating comments

--- server/server.js
+++ server/server.js
@@ -1,17 +1,45 @@
 import fastify from "fastify";
 import sensible from "@fastify/sensible";
 import cors from "@fastify/cors";
+import cookie from "@fastify/cookie"
 import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 dotenv.config()
 
 const app = fastify()
 app.register(sensible)
+app.register(cookie, { secret: process.env.COOKIE_SECRET})
 app.register(cors, {
     origin: process.env.CLIENT_URL,
     credentials: true,
 })
+
+app.addHook("onRequest", (req, res, done) => {
+  if (req.cookies.userId != CURRENT_USER_ID) {
+    req.cookies.userId = CURRENT_USER_ID
+    res.clearCookie("userId")
+    res.setCookie("userId", CURRENT_USER_ID)
+  }
+  done()
+})
 const prisma = new PrismaClient()
+
+const CURRENT_USER_ID = (
+  await prisma.user.findFirst({ where: { username: "juliusomo" } })
+).id
+
+const COMMENT_SELECT_FIELDS = {
+  id: true,
+  message: true,
+  parentId: true,
+  createdAt: true,
+  user: {
+    select: {
+      id: true,
+      username: true,
+    },
+  },
+}
 
 app.get("/posts", async (req, res) => {
   return await commitToDb(
@@ -35,23 +63,33 @@ app.get("/posts/:id", async (req, res) => {
                 orderBy: {
                     createdAt: "desc"
                 },
-                select: {
-                    id: true,
-                    message: true,
-                    parentId: true,
-                    createdAt: true,
-                    user: {
-                        select: {
-                            id: true,
-                            username: true
-                        }
-                    }
-                }
-            }
-        }
+                select: COMMENT_SELECT_FIELDS
+            },
+        },
     })
   )
 })
+
+
+app.post("/posts/:id/comments", async (req, res) => {
+  if (req.body.message === "" || req.body.message == null) {
+    return res.send(app.httpErrors.badRequest("Message is required")
+    )
+  }
+
+  return await commitToDb(
+    prisma.comment.create({
+      data: {
+        message: req.body.message,
+        userId: req.cookies.userId,
+        parentId: req.body.parentId,
+        postId:req.params.id
+      },
+      select: COMMENT_SELECT_FIELDS
+    })
+  )
+})
+
 
 
 
