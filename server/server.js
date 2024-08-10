a562c50c8924acbd487a5ae6c0d7ee972b700425
danielaebazan <daniela.e.bazan@hotmail.com>
2024-08-10 13:16:36 -0600

    test

--- server/server.js
+++ server/server.js
@@ -6,11 +6,12 @@ import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 dotenv.config()
 
+const app = fastify()
+
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-const app = fastify()
 app.register(sensible)
 app.register(cookie, { secret: process.env.COOKIE_SECRET})
 app.register(cors, {
