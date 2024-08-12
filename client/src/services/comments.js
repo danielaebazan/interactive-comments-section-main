21ed191d9b0de73ae175ae3407dc0e531be5c8f7
danielaebazan <daniela.e.bazan@hotmail.com>
2024-08-12 11:58:33 -0600

    new update

--- client/src/services/comments.js
+++ client/src/services/comments.js
@@ -1,27 +1,29 @@
 import { makeRequest } from "./makeRequest"
 
 export function createComment({ postId, message, parentId }) {
-  return makeRequest(`posts/${postId}/comments`, {
+  return makeRequest(`posts/${postId}`, {
     method: "POST",
     data: { message, parentId },
   })
 }
 
 export function updateComment({ postId, message, id }) {
-  return makeRequest(`posts/${postId}/comments/${id}`, {
+  return makeRequest(`posts/${postId}`, {
     method: "PUT",
-    data: { message },
+    data: { commentId: id, message },
   })
 }
 
 export function deleteComment({ postId, id }) {
-  return makeRequest(`posts/${postId}/comments/${id}`, {
+  return makeRequest(`posts/${postId}`, {
     method: "DELETE",
+    data: { commentId: id },
   })
 }
 
 export function toggleCommentLike({ id, postId }) {
-  return makeRequest(`/posts/${postId}/comments/${id}/toggleLike`, {
+  return makeRequest(`posts/${postId}/toggleLike`, {
     method: "POST",
+    data: { commentId: id },
   })
 }
